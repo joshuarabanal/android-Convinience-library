54fb1616e761637a6c4f913f1976dc6106bd7b2b
joshuarabanal <joshuarabanal@gmail.com>
2019-08-25 11:27:15 -0400

    message

--- java/android/run.java
+++ java/android/run.java
@@ -1,13 +1,13 @@
+package android;
+
 import android.app.Activity;
 import android.content.Intent;
 
 public class run {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Activity a = new Activity(new Intent());
 		a.performStart();
 		
 	}
-
 }
